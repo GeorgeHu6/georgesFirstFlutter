23b6083ec9ac188e9a1935fc745bef5e416e44d1
Latnx <969163368@qq.com>
2022-06-16 16:24:43 +0800

    add drinkInfo

--- lib/models/drinkingInfo.g.dart
+++ lib/models/drinkingInfo.g.dart
@@ -6,8 +6,20 @@ part of 'drinkingInfo.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-DrinkingInfo _$DrinkingInfoFromJson(Map<String, dynamic> json) =>
-    DrinkingInfo();
+DrinkingInfo _$DrinkingInfoFromJson(Map<String, dynamic> json) => DrinkingInfo()
+  ..age = json['age'] as String?
+  ..height = json['height'] as String?
+  ..weight = json['weight'] as String?
+  ..drinkWaterToday = json['drinkWaterToday'] as String?
+  ..calories = json['calories'] as String?
+  ..schedule = json['schedule'] as List<dynamic>?;
 
 Map<String, dynamic> _$DrinkingInfoToJson(DrinkingInfo instance) =>
-    <String, dynamic>{};
+    <String, dynamic>{
+      'age': instance.age,
+      'height': instance.height,
+      'weight': instance.weight,
+      'drinkWaterToday': instance.drinkWaterToday,
+      'calories': instance.calories,
+      'schedule': instance.schedule,
+    };
