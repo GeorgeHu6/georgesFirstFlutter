245cc54e5666e2e6bbeab147c42340e5d4345bda
GeorgeHu <43069137+GeorgeHu6@users.noreply.github.com>
2022-06-30 09:04:49 +0800

    Update home_page.dart

--- lib/routes/home_page.dart
+++ lib/routes/home_page.dart
@@ -3,7 +3,6 @@ import 'package:percent_indicator/percent_indicator.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import '../models/drinkingInfo.dart';
 
-DrinkingInfo drinkingInfo = DrinkingInfo();
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
